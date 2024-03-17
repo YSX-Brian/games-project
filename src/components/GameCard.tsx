9b772a7d885cd2bfc3049348ec46cec391669dc0
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2024-03-16 21:23:24 -0400

    Remove card and skeleton duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,8 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     //overflow accounts for the img being bigger than its container
-    <Card borderRadius={10} overflow={"hidden"} width="300px">
+
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
